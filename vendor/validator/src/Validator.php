7a325d3be421b5274ac00a75d64a311493901eb9
Rahman <dikaevr053@gmail.com>
2020-10-07 15:59:14 +0300

    Добавил функционал:    до начала валидации, я сделал проверку пришедших данных, т.е если в правилах на поле указано "require" => true - это означает что он обязателен, то валидатор будет проверят, пришел ли такое поле, если не пришел выдаст ошибку    сделал функцию которая заполняет массив ошибок - $this->_error, все ошибки будут хранятся там
    Исправил баги:
        не правильно установил язык, т.е язык не закачивался

--- vendor/validator/src/Validator.php
+++ vendor/validator/src/Validator.php
@@ -1,23 +1,24 @@
 <?php
 namespace Validator;
 
-class Validator
+final class Validator
 {
     private $_rules = [];
     private $_fields = [];
     private $_languages = [];
+    private $_requiredFields = [];
     private $_lang;
     private $_errors = [];
     private $messagesFields = [];
 
     public function __construct(array $rules = [], $_lang = 'ru') {
-        $this->languages = require __DIR__ . '/languages/language.php';
+        $this->_languages = require __DIR__ . '/languages/language.php';
         $this->rules($rules);
         $this->setLanguage($_lang);
     }
 
     public function validate(array $data) {
-
+        $data = $this->existsRequireFields($data);
     }
 
     public function rules(array $rules = []) {
@@ -30,7 +31,7 @@ class Validator
     }
 
     protected function existsLanguage($langName) {
-        if (array_key_exists($langName, $this->_languages))
+        if (!array_key_exists($langName, $this->_languages))
             throw new \Exception('You chose a non-existent language');
         else
             return true;
@@ -46,7 +47,33 @@ class Validator
         });
     }
 
+    protected function existsRequireFields($data) {
+        if (!empty(array_column($this->_rules, 'require'))) {
+            foreach ($this->_rules as $rule => $val) {
+                if (array_key_exists('require', $val) && $val['require'] !== false) {
+                    if (!array_key_exists($rule, $data)){
+                        $this->error($rule, "require");
+                    }
+                }
+            }
 
+        }
+    }
+
+    protected function error($field, $stringKey, $val = '') {
+        $nowLang = $this->_languages[$this->_lang];
+        if (array_key_exists($stringKey, $nowLang)) {
+            $this->_errors[$field][] = sprintf($nowLang[$stringKey], $val);
+            return true;
+        }
+        throw new Exception('Error');
+    }
+
+    protected function dd($arr) {
+        echo '<pre>';
+        print_r($arr);
+        echo '</pre>';
+    }
 
     /**
      * @return array
